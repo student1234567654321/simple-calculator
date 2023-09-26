310279bdcca8e9c402509196736e7625f9a2b41c
student1234567654321 <giovanni.beltran@siu.edu>
2023-09-26 15:30:37 -0500

    Implemented division feature

--- calculator.c
+++ calculator.c
@@ -16,10 +16,21 @@ int mul(double x, double y) {
 	return result;
 }
 
+int div(double x, double y) {
+	if (y != 0) {
+		return x / y;
+	}
+	else {
+		printf("\nError: Division by zero. Ignore next printed line.\n");
+		return 0;
+	}
+}
+
 int main() {
-	double a = 9;
-	double b = 10;
+	double a = 18;
+	double b = 0;
 	printf("The sum of A and B is: %d\n", add(a,b));
 	printf("The difference of A and B is: %d\n", sub(a,b));
 	printf("The product of A and B is: %d\n", mul(a,b));
+	printf("The quotient of A and B is: %d\n", div(a,b));
 }
