4aab540d6f934ff549573ed3df7524d561d3bf43
student1234567654321 <giovanni.beltran@siu.edu>
2023-09-26 15:20:14 -0500

    Implemeneted subtraction feature

--- calculator.c
+++ calculator.c
@@ -6,8 +6,14 @@ int add(double x, double y) {
 	return result;
 }
 
+int sub(double x, double y) {
+	double result = x - y;
+	return result;
+}
+
 int main() {
 	double a = 9;
 	double b = 10;
-	printf("The sum of A and B is: %d", add(a,b));
+	printf("The sum of A and B is: %d\n", add(a,b));
+	printf("The difference of A and B is: %d\n", sub(a,b));
 }
