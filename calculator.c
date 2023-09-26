14d25acdc0f78c2ce69e46725ba2fa47ca11300b
student1234567654321 <giovanni.beltran@siu.edu>
2023-09-26 15:08:52 -0500

    Implemented addition feature

--- calculator.c
+++ calculator.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <math.h>
 
+int add(double x, double y) {
+	double result = x + y;
+	return result;
+}
+
 int main() {
-	return 0;
+	double a = 9;
+	double b = 10;
+	printf("The sum of A and B is: %d", add(a,b));
 }
